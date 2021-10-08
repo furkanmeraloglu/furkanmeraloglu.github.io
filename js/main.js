fcdd41aae6027d46b037768477eb12defb6fc268
Furkan Meraloglu <furkanmeraloglu@gmail.com>
2021-10-08 22:12:37 +0300

    Header finalized

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 900){
+            $('.navbar').addClass("sticky");
+        }else {
+            $('.navbar').removeClass("sticky");
+        }
+    })
+})
+
+var typed = new Typed(".typing",{
+    strings:["Merhaba!", "Hello!", "Hola!", "Bonjour!", "Hallo!"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
