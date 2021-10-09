562b74f94a7fa2e9be20af72493578e9ce51e0ed
Furkan Meraloglu <furkanmeraloglu@gmail.com>
2021-10-10 00:37:10 +0300

    About and education sections added

--- js/main.js
+++ js/main.js
@@ -1,16 +1,65 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 400){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 300) {
             $('.navbar').addClass("sticky");
-        }else {
+        } else {
             $('.navbar').removeClass("sticky");
         }
     })
 })
 
-var typed = new Typed(".typing",{
-    strings:["Merhaba!", "Hello!", "Hola!", "Bonjour!", "Hallo!"],
+var typed = new Typed(".typing", {
+    strings: ["Merhaba!", "Hello!", "Hola!", "Bonjour!", "Hallo!"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
 })
+
+$('.navbar-collapse a').click(function () {
+    $(".navbar-collapse").collapse('hide');
+});
+
+
+
+
+
+/* IE/Safari Scroll Smoothness */
+
+$('.navbar a').on('click', function(e){
+    if(this.hash !== ''){
+        e.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate({
+           scrollTop: $(hash).offset().top
+        }, 700);
+    }
+})
+
+/* Owl Carousel for Edu-Info */
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav:true,
+    navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        750:{
+            items:2,
+            nav:false
+        },
+        1200:{
+            items:3,
+            nav:true,
+            loop:true,
+            animateOut: true
+        }
+    }
+});
